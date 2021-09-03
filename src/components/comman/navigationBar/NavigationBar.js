49917fd1bd8595511a42a4324fb1f9dea0ffcf6c
Pratik <pranavhonmode123@gmail.com>
2021-09-03 22:54:26 +0530

    Added navigation bar icons

---
+++ src/components/comman/navigationBar/NavigationBar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Badge, Grid, Hidden, Typography, FormHelperText } from "@material-ui/core";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { BsFillBellFill } from "react-icons/bs";
+import {FaUserAlt} from 'react-icons/fa';
+
+const NavigationBar = () => {
+  return (
+    <Grid container xs={12}>
+      <Grid item xs={12} className="nav-bar-cnt flex align-items-center">
+        <Hidden only={["md", "lg", "xl"]}>
+          <GiHamburgerMenu className="menu-icon" />
+        </Hidden>
+        <Grid item xs={3}>
+          <Typography className="nav-logo">workcanios</Typography>
+        </Grid>
+        <Grid item xs={9} className="flex nav-icons-cnt">
+          <div className="nav-icons-cnt-1">
+            <FaUserAlt className="nav-icons" />
+            <Hidden only={['xs', 'sm']}>
+              <FormHelperText className="nav-icons-cnt-1-text">Hi Pranav!</FormHelperText>
+            </Hidden>
+          </div>
+          <div className="nav-icons-cnt-1">
+            <Badge badgeContent={6} color="secondary">
+              <BsFillBellFill className="nav-icons" />
+            </Badge>
+            <Hidden only={['xs', 'sm']}>
+              <FormHelperText className="nav-icons-cnt-1-text">notifications</FormHelperText>
+            </Hidden>
+          </div>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default NavigationBar;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import Footer from "../components/comman/Footer";
-import NavigationBar from "../components/comman/NavigationBar";
+import NavigationBar from "../components/comman/navigationBar/NavigationBar";
 import Home from "../components/main/Home";
 
 const AppRoutes = () => {
